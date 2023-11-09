c9c1e9c8c2b056136bfaeb7177678a55e6dcdd89
faelpereira10 <131384531+faelpereira10@users.noreply.github.com>
2023-11-09 10:38:32 -0300

    Add files via upload

---
+++ backprogrammobile/app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const mysql = require('mysql2');
+var cors = require('cors')
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'cardapio'
+});
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Alo mundo!')
+})
+
+app.get('/categoria', (req, res) => {
+    // simple query
+    connection.query(
+        'SELECT * FROM categoria',
+        function (err, results, fields) {
+            res.send(results);
+        }
+    );
+})
+
+app.post('/categoria_inserir', (req, res) => {
+    // simple query
+    connection.query(
+        'insert into categoria(titulo) values("Comida Boa")',
+        function (err, results, fields) {
+            res.send(results);
+        }
+    );
+})
+
+app.patch('/categoria_atualizar/:id', (req, res) => {
+    // simple query
+    var id_categoria = req.params.id
+    connection.query(
+        'update categoria set titulo = "Comida Ruim" where id_categoria = ' + id_categoria,
+        function (err, results, fields) {
+            console.log(err)
+            res.send(results);
+        }
+    );
+})
+
+app.post('/categoria_apagar/:id', (req, res) => {
+    // simple query
+    var id_categoria = req.params.id
+    connection.query(
+        'delete from categoria where id_categoria = ' + id_categoria,
+        function (err, results, fields) {
+            console.log(err)
+            res.send(results);
+        }
+    );
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
